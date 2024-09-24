6a4ba578fac055b37f8550dd86b224965e7a3cb9
Megagig <megagigsoftwaresolutions.com>
2024-09-24 17:39:15 +0100

    Tailwind Responsive Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>test{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
