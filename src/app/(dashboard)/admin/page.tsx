be69bb2ece4a94b22fdc48a0afc34e34214fb403
Megagig <megagigsoftwaresolutions.com>
2024-09-26 10:18:13 +0100

    Add the UserCard component on the Admin Dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,22 @@
+import UserCard from '@/components/UserCard';
+
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="Staff" />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">R</div>
+    </div>
+  );
 };
 
 export default AdminPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        lamaSky: '#C3EBFA',
+        lamaSkyLight: '#EDF9FD',
+        lamaPurple: '#CFCEFF',
+        lamaPurpleLight: '#F1F0FF',
+        lamaYellow: '#FAE27C',
+        lamaYellowLight: '#FEFCE8',
       },
     },
   },
