4d2c004dcf3c9e779f50e54028ca50a1c0749f66
Megagig <megagigsoftwaresolutions.com>
2024-09-26 17:01:59 +0100

    Finish the attendance barchart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import CountChart from '@/components/CountChart';
 import UserCard from '@/components/UserCard';
+import AttendanceChart from '@/components/AttendanceChart';
 
 const AdminPage = () => {
   return (
@@ -20,7 +21,9 @@ const AdminPage = () => {
             <CountChart />
           </div>
           {/* ATTENDANCE CHART */}
-          <div className="w-full lg:w-2/3 h-[450px]"></div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
         </div>
         {/* BOTTOM CHART */}
         <div className=""></div>
