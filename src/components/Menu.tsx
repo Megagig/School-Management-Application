d12d6b15124c1ee13dde3931f3a660f62dc3022c
Megagig <megagigsoftwaresolutions.com>
2024-09-30 15:40:14 +0100

    Finish the AdminDashboard design

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -124,16 +125,20 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {item.title}
           </span>
-          {item.items.map((item) => (
-            <Link
-              href={item.href}
-              key={item.label}
-              className="flex items-center justify-center lg: justify-start gap-4 text-gray-500 py-2"
-            >
-              <Image src={item.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {item.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       ))}
     </div>
