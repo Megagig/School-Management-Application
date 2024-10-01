add9d3c9823b0c8fa265c7e4de3db5d2336849a8
Megagig <megagigsoftwaresolutions.com>
2024-10-01 10:01:59 +0100

    Add the TableSearch component

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,33 @@
+import TableSeach from '@/components/TableSeach';
+import Image from 'next/image';
+
+const TeacherListPage = () => {
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSeach />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/plus.png" alt="" width={14} height={14} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <div className=""></div>
+      {/* PAGINATION */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default TeacherListPage;
