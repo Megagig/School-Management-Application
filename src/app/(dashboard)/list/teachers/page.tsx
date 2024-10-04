7b13203beb061b10adf588be63cb1f5c0aa0483f
Megagig <megagigsoftwaresolutions.com>
2024-10-04 18:29:29 +0100

    Search filter

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import TableSeach from '@/components/TableSearch';
 import { role, teachersData } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Class, Subject, Teacher } from '@prisma/client';
+import { Class, Subject, Teacher, Prisma } from '@prisma/client';
 import prisma from '@/lib/prisma';
 import { ITEM_PER_PAGE } from '@/lib/setting';
 
@@ -97,8 +97,34 @@ const TeacherListPage = async ({
   const { page, ...queryParams } = searchParams;
   const p = page ? parseInt(page) : 1;
 
+  // URL PARAMS CONDITION
+
+  const query: Prisma.TeacherWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case 'classId':
+            query.lessons = {
+              some: {
+                classId: parseInt(value),
+              },
+            };
+            break;
+          case 'search':
+            query.name = { contains: value, mode: 'insensitive' };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
   const [teachers, count] = await prisma.$transaction([
     prisma.teacher.findMany({
+      where: query,
       include: {
         subjects: true,
         classes: true,
@@ -106,7 +132,7 @@ const TeacherListPage = async ({
       take: ITEM_PER_PAGE,
       skip: ITEM_PER_PAGE * (p - 1),
     }),
-    prisma.teacher.count(),
+    prisma.teacher.count({where: query}),
   ]);
 
   return (
